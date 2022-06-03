97037ac7e811270f515f93ba2018fd4187f1e403
Louis Singer <41042567+louisinger@users.noreply.github.com>
2022-06-03 18:01:33 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script type="ts">
+  import Connect, { marinaStore, MarinaStore } from 'svelte-marina-button';
+
+  // You can subscribe to marina status changes
+  marinaStore.subscribe((s: MarinaStore) => {
+    console.log(`Marina is attached to the ${s.network} network`);
+  });
+</script>
+
+<div class="container is-widescreen">
+  <div class="box">
+    <h1 class="title">Hello fren!</h1>
+    <Connect cssClass={'button'} />
+  </div>
+</div>
+
+<style src="./scss/main.scss" lang="scss" global>
+</style>
